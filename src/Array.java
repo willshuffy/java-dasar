54c6e3a60a02b4cd93d4d92a2029807ce2f63b37
willshuffy <saputrawilly26@gmail.com>
2021-07-15 16:02:43 +0700

    Array Initializer

--- src/Array.java
+++ src/Array.java
@@ -17,5 +17,28 @@ public class Array {
         stringArray[0] = "Bruce";
 
         System.out.println(stringArray[0]);
+
+
+        /*
+            Array initializer
+         */
+
+
+        //cara 1
+        String[] namaHewan = {
+                "ayam", "bebek", "kambing", "sapi", "kerbau"
+        };
+
+
+        //cara 2
+        int[] arrayInt = new int[]{
+                1,2,3,4,5,6,7,8,9
+        };
+
+
+        //cara 3
+        long[] arrayLong = new long[]{
+                10L, 20L, 30L
+        };
     }
 }
