4f7f62e2f6dd60d314185212eea0c65a6d2c5a7a
willshuffy <saputrawilly26@gmail.com>
2021-07-15 14:04:04 +0700

    Variable

---
+++ src/Vasriable.java
@@ -0,0 +1,20 @@
+public class Vasriable {
+
+    public static void main(String[] args) {
+
+        //contoh variable string
+        String name;
+        name = "willy saputra";
+        String address = "indonesia";
+
+        System.out.println(name);
+        System.out.println(address);
+
+        //contoh variable integer
+
+        int age;
+        age = 31;
+
+        System.out.println(age);
+    }
+}
