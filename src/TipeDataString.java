55fb804f255d5ae11c1d4cac0bbc144fd5e6067c
willshuffy <saputrawilly26@gmail.com>
2021-07-15 13:51:13 +0700

    Tipe Data String

---
+++ src/TipeDataString.java
@@ -0,0 +1,18 @@
+public class TipeDataString {
+
+    public static void main(String[] args) {
+
+        String firstName = "Willy saputra";
+        String lastName = "Saputra";
+
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+
+        //penggabungan string
+        String fullName = firstName +" "+ lastName;
+
+        System.out.println(fullName);
+    }
+}
