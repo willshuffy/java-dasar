4827e186a46d20e9511c2009617653cbe0f328d8
willshuffy <saputrawilly26@gmail.com>
2021-07-15 11:01:47 +0700

    Tipe Data Char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,18 @@
+public class TipeDataChar {
+
+    public static void main(String[] args) {
+
+        char w = 'W';
+        char i = 'I';
+        char l = 'L';
+
+        System.out.println(w);
+        System.out.println(i);
+        System.out.println(l);
+
+
+        System.out.print(w);
+        System.out.print(i);
+        System.out.print(l);
+    }
+}
