20f181c60d3a5b14f9a9fa43d850b5ec205638a7
willshuffy <saputrawilly26@gmail.com>
2021-07-16 16:36:31 +0700

    IF Else Statement

---
+++ src/IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        /*
+            IF Else statement
+         */
+
+        int nilai = 80;
+        int absen = 90;
+
+        if (nilai >= 75 && absen >= 75){
+            System.out.println("selamat anda lulus");
+        } else {
+            System.out.println("silahkan coba lagi tahun depan");
+        }
+    }
+}
