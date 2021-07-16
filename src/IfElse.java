e96de3081760dd4f470586b36a44a09a7ab6e5df
willshuffy <saputrawilly26@gmail.com>
2021-07-16 16:55:39 +0700

    Else if statement

--- src/IfElse.java
+++ src/IfElse.java
@@ -14,5 +14,19 @@ public class IfElse {
         } else {
             System.out.println("silahkan coba lagi tahun depan");
         }
+
+        /*
+            Els IF Statement
+         */
+
+        if (nilai >= 90 && absen >= 90){
+            System.out.println("Nilai anda A");
+        }else if (nilai >= 80 && absen >= 80){
+            System.out.println("Nilai anda B");
+        }else if (nilai >= 70 && absen >= 70){
+            System.out.println("Nilai anda C");
+        }else {
+            System.out.println("Nilai anda D");
+        }
     }
 }
