58c68d8a00a5445e8b0b8f049891c839000df7de
willshuffy <saputrawilly26@gmail.com>
2021-07-16 14:30:27 +0700

    operasi matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,14 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 10;
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
