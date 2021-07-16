4b0d3b55830e8b5df49bba9e2589dab0292468bd
willshuffy <saputrawilly26@gmail.com>
2021-07-16 14:30:48 +0700

    Aughmented  Assignment

--- src/OperasiMatematika.java
+++ src/OperasiMatematika.java
@@ -10,5 +10,42 @@ public class OperasiMatematika {
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(a % b);
+
+
+        /*
+            Aughmented assignment
+            operasi matematika dengan dirinya sendiri
+         */
+
+        a = 100;
+        b = 100;
+
+
+        //operasi mtk biasa
+        a = a + 10;
+        System.out.println(a);
+
+        //aughmented assignment
+        b += 10;
+        System.out.println(b);
+
+
+        //operasi mtk biasa
+        a = a - 10;
+        System.out.println(a);
+
+        //aughmented assignment
+        b -= 10;
+        System.out.println(b);
+
+
+        //operasi mtk biasa
+        a = a * 10;
+        System.out.println(a);
+
+        //aughmented assignment
+        b *= 10;
+        System.out.println(b);
+
     }
 }
