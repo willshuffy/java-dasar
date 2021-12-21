f62579350237551a18206207df13ce2af350e3bd
willshuffy <saputrawilly26@gmail.com>
2021-12-21 14:23:11 +0700

    WhileLoop

---
+++ src/WhileLoop.java
@@ -0,0 +1,20 @@
+public class WhileLoop {
+
+    /**
+     * time : 2:39:06
+     *
+     * While Loop => versi perulangan yang lebih sederhana dibanding for loop
+     * dan hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+     */
+
+    public static void main(String[] args) {
+
+        var counter = 1;
+
+        while (counter <= 10){
+            System.out.println("perulangan" + counter);
+
+            counter++;
+        }
+    }
+}
