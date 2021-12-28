8313ec8cc1d9424e9928b1376a7ab10ac56aa2f4
willshuffy <saputrawilly26@gmail.com>
2021-12-28 12:04:07 +0700

    Continue

--- src/BreakandContinue.java
+++ src/BreakandContinue.java
@@ -1,9 +1,9 @@
 public class BreakandContinue {
 
     /**
-     *
-     * ============================== Break & Continue ==============================
-     *
+     * ============================== Break ==============================
+     * <p>
+     * time : 2:44:49
      * Break pada perulangan digunakan untuk menghentikan seluruh perulangan
      * namun berbeda dengan continue, continue digunakan untuk menghentikan perulangan saat ini, lalu melanjutkan
      * ke perulangan selanjutnya
@@ -13,15 +13,32 @@ public class BreakandContinue {
 
         var counter = 1;
 
-        while (true){
+        while (true) {
             System.out.println("Perulangan " + counter);
             counter++;
 
-            if (counter > 10){
+            if (counter > 10) {
                 break;
             }
         }
 
         System.out.println("Perulangan berhenti");
+
+
+        /**
+         *
+         * ============================== Continue ==============================
+         *
+         * time : 2:48:26
+         */
+
+        for (var counter2 = 1; counter2 <= 100; counter2++) {
+
+            if (counter2 % 2 == 0){ //jika genap maka == 1
+                continue;
+            }
+
+            System.out.println("Perulangan Ganjil " + counter2);
+        }
     }
 }
