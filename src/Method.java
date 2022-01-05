cad4daec32d03884f488e2cbb2b0dc4f7387f601
willshuffy <saputrawilly26@gmail.com>
2022-01-05 14:10:56 +0700

    Method

---
+++ src/Method.java
@@ -0,0 +1,22 @@
+public class Method {
+
+    /**
+     * ============================== Method ==============================
+     * <p>
+     * time : 2:55:32
+     * <p>
+     * Di bahasa pemrograman lain, Method juga disebut dengan Function
+     * Method adalah blok kode program yang akan berjalan saat kita panggil
+     * Untuk membuat method di java, kita bisa menggunakan kata kunci void, lalu diikuti dengan nama method, kurung ()
+     */
+
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+    static void sayHelloWorld() {
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+    }
+}
