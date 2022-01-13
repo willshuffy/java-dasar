d14f657a7204bdb1dcd2049fd7a0ae3eb215a16d
willshuffy <saputrawilly26@gmail.com>
2022-01-13 12:24:49 +0700

    Method Return Value

--- src/Method.java
+++ src/Method.java
@@ -16,6 +16,16 @@ public class Method {
         //[STEP 2]
         sayHelloName("Willy", "Saputra");
         sayHelloName("Bruce", "Wayne");
+
+        //[STEP 2]
+        var result1 = sum(100, 100);
+        System.out.println(result1);
+        System.out.println(sum(200, 200));
+
+        //[STEP 4]
+        System.out.println(hitung(500, "+", 100));
+        System.out.println(hitung(2000, "-", 100));
+        System.out.println(hitung(200, "salah", 100));
     }
 
     static void sayHelloWorld() {
@@ -40,5 +50,40 @@ public class Method {
     //[STEP 1]
     static void sayHelloName(String firstName, String lastName){
         System.out.println("Hello " + firstName + " " + lastName);
+    }
+
+    /**
+     *
+     * ============================== Method Return Value ==============================
+     *
+     * time : 3:03:27
+     *
+     * Secara default, method itu tidak menghasilkan value apapun, namun jika kita ingin, kita bisa membuat sebuah
+     * ~method mengembalikan nilai
+     * Agar method bisa menghasilkan value, kita harus mengubah kata kunci void dengan tipe data yang dihasilkan
+     * Dan didalam block method, untuk menghasilkan nilai tersebut, kita harus menggunakan kata kunci return, lalu
+     * ~diikuti dengan data yang sesuai dengan tipe data yang sudah kita deklarasikan di method
+     * Di Java, kita hanya bisa menghasilkan 1 data di sebuah method, tidak bisa lebih dari satu
+     *
+     */
+
+    //[STEP 1]
+    static int sum(int value1, int value2){
+        var total = value1 + value2;
+        return total;
+    }
+
+
+    //[STEP 3] --------- menggunakan return value lebih dari satu ---------
+
+    static int hitung(int value1, String operasi, int value2){
+        switch (operasi){
+            case "+":
+                return value1 + value2;
+            case "-":
+                return value1 - value2;
+            default:
+                return 0;
+        }
     }
 }
